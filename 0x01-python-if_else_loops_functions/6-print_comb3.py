f739ce5b80d4d76d884dabf86030b859f993fddf
omvriio <unimarghadi@gmail.com>
2023-12-05 03:07:14 -0800

    d

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(89):
-    if ((i%10) is not (i-i%10)) and i%10 > (i-i%10):
+    if ((i%10) is not (i-i%10)) and ((i%10) > (i-i%10)):
         print('{:02d}, '.format(i), end='')
 print('89')
